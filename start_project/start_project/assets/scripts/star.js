de78d9c00683458278e7150b9944f29256dcd05a
Hakurei Reimu <Hakurei__Reimu@outlook.com>
2018-07-30 11:34:48 +0800

    201807301134

---
+++ start_project/start_project/assets/scripts/star.js
@@ -0,0 +1,29 @@
+//star.js
+cc.Class
+    (
+    {
+        extends: cc.Component,
+
+        properties:
+        {
+            // 星星和主角之间的距离小于这个数值时，就会完成收集
+            pickRadius: 0,
+            // 暂存 Game 对象的引用
+            game: {
+                default: null,
+                serializable: false
+            }
+        },
+
+        // LIFE-CYCLE CALLBACKS:
+
+        // onLoad () {},
+
+        start()
+        {
+
+        },
+
+        // update (dt) {},
+    }
+    );
---
+++ start_project/start_project/library/imports/4a/4a2ed775-10b9-42c3-ab22-3df96719478a.js
@@ -0,0 +1,31 @@
+"use strict";
+cc._RF.push(module, '4a2edd1ELlCw6siPflnGUeK', 'star');
+// scripts/star.js
+
+"use strict";
+
+//star.js
+cc.Class({
+        extends: cc.Component,
+
+        properties: {
+                // 星星和主角之间的距离小于这个数值时，就会完成收集
+                pickRadius: 0,
+                // 暂存 Game 对象的引用
+                game: {
+                        default: null,
+                        serializable: false
+                }
+        },
+
+        // LIFE-CYCLE CALLBACKS:
+
+        // onLoad () {},
+
+        start: function start() {}
+}
+
+// update (dt) {},
+);
+
+cc._RF.pop();
