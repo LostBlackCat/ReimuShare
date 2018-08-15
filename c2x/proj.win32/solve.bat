3bf7133d509f4aae4bb8b21404746f956c0fa16b
Hakurei Reimu <Hakurei__Reimu@outlook.com>
2018-08-15 17:01:23 +0800

    201808151701 Add the test cocos project and modify some files.

---
+++ c2x/proj.win32/solve.bat
@@ -0,0 +1,6 @@
+@echo off
+xcopy /y /e /h Classes\reader proj.win32\reader <ENTER
+xcopy /y /e /h Resource\creator proj.win32\creator <ENTER
+
+
+ 
---
+++ c2x/solve.bat
@@ -0,0 +1,6 @@
+@echo off
+xcopy /y /e /h  Classes\reader proj.win32\reader
+xcopy /y /e /h  Resources\creator proj.win32\creator
+
+
+ 
